ac764ca568fcb60bc699cfbbff0b9d663edde2a3
Tirthankar <tirthankar@demansol.com>
2018-02-21 14:58:57 +0530

    Added picker view

--- GoodsOwner/GoodsOwner/Classes/Controller/Login/GORegistrationVC.swift
+++ GoodsOwner/GoodsOwner/Classes/Controller/Login/GORegistrationVC.swift
@@ -10,7 +10,10 @@ import UIKit
 
 
 
-class GORegistrationVC: GOBaseVC {
+class GORegistrationVC: GOBaseVC, GODropDownViewDelegate {
+    
+    var _categoryView : GODropDownView!
+    var _transparentView : UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,10 +30,44 @@ class GORegistrationVC: GOBaseVC {
         let controller = self.storyboard?.instantiateViewController(withIdentifier: "GORegistrationIntermediateVC")
         self.navigationController?.pushViewController(controller!, animated: true)
     }
+    
+    @IBAction func categoryButtonPressed(_ sender: UIButton) {
+        
+        _transparentView = UIView();
+        _transparentView.backgroundColor = UIColor.black;
+        _transparentView.alpha = 0.6
+        _transparentView.frame = self.view.frame
+        
+        self.view.addSubview(_transparentView)
+        
+        _categoryView = UINib(nibName: "GODropDownView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! GODropDownView
+        _categoryView.delegate = self
+        
+        _categoryView.bounds.size.width = self.view.bounds.size.width
+        _categoryView.bounds.size.height = 287
+        _categoryView.frame.origin = CGPoint.init(x: 0, y: self.view.bounds.size.height - 287)
+        _categoryView.pickerDataSource = ["Category 1", "Category 2", "Category 3",   "Category 4", "Category 5"]
+        
+        self.view.addSubview(_categoryView)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: GODropDownViewDelegate
+    func donePressed() {
+        
+        if _transparentView != nil {
+            _transparentView.removeFromSuperview()
+            _transparentView = nil
+        }
+        
+        if _categoryView  != nil {
+            _categoryView.removeFromSuperview()
+            _categoryView = nil
+        }
     }
 }
 
---
+++ GoodsOwner/GoodsOwner/Classes/Views/PickerViews/GODropDownView.swift
@@ -0,0 +1,53 @@
+//
+//  GODropDownView.swift
+//  GoodsOwner
+//
+//  Created by Nabanita on 21/02/18.
+//  Copyright © 2018 company. All rights reserved.
+//
+
+import UIKit
+
+protocol GODropDownViewDelegate {
+    func donePressed()
+}
+
+class GODropDownView: UIView {
+    
+    @IBOutlet weak var _headerTitle: UILabel!
+    @IBOutlet weak var _pickerView: UIPickerView!
+    
+    var delegate: GODropDownViewDelegate?
+    var selectedPickerValue: String?
+    
+    var pickerDataSource = [String]()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: UIButton) {
+        delegate?.donePressed()
+    }
+
+}
+
+extension GODropDownView: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerDataSource[row] as String
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(pickerDataSource[row])
+        selectedPickerValue = pickerDataSource[row]
+    }
+}
