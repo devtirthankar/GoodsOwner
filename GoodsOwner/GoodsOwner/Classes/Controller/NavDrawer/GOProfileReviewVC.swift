4676df5bd04606de86908388dff9d4483cf7cb66
Nabanita <nabanita@demansol.com>
2018-02-22 10:03:50 +0530

    With review cells..

--- GoodsOwner/GoodsOwner/Classes/Controller/NavDrawer/GOProfileReviewVC.swift
+++ GoodsOwner/GoodsOwner/Classes/Controller/NavDrawer/GOProfileReviewVC.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class GOProfileReviewVC: GOBaseVC {
     
-    let cellStoreIdentifier = "GDStoreReviewCell"
+    @IBOutlet weak var _collectionView: UICollectionView!
+    let cellStoreIdentifier = "GOProfileReviewCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        _collectionView.register(UINib.init(nibName: cellStoreIdentifier, bundle: nil), forCellWithReuseIdentifier: cellStoreIdentifier)
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,4 +37,44 @@ class GOProfileReviewVC: GOBaseVC {
         
         print("Review View Controller Will Disappear")
     }
+}
+
+extension GOProfileReviewVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        
+        let cell: GOProfileReviewCell = _collectionView.dequeueReusableCell(withReuseIdentifier: cellStoreIdentifier, for: indexPath) as! GOProfileReviewCell
+            
+            cell.commentLabel.text = "Review \(indexPath.row + 1)"//descriptions[indexPath.row]
+            return cell;
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        /*
+        if let cell = GOProfileReviewCell.fromNib() {
+                
+                let preferredSize = cell.preferredLayoutSizeFittingWidth(targetWidth: _collectionView.frame.size.width - 6)
+                return preferredSize
+        }*/
+        return CGSize.init(width: _collectionView.frame.size.width, height: CGFloat(140.0))//CGSize.zero
+    }
+    
+    
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        let edgeInset = UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 0)
+        return edgeInset
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat{
+        return 3
+    }
+    
 }
